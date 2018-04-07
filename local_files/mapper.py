fc5f565ea4eb57dea1f62f17ffd7bfbe8e3b8fd3
HeaveyA <a.heavey2@gmail.com>
2018-04-07 18:15:15 +0100

    Update mapper.py

--- local_files/mapper.py
+++ local_files/mapper.py
@@ -1,4 +1,3 @@
-.
 #!/usr/bin/env python
 import sys
  
@@ -13,5 +12,3 @@ for line in sys.stdin:
     #--- output tuples [word, 1] in tab-delimited format---
     for word in words: 
         print '%s\t%s' % (word, "1")
-
-.
