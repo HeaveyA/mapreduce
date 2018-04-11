6a143b377f0f49d4a1a70a4788ba59e7b2408733
HeaveyA <a.heavey2@gmail.com>
2018-04-11 20:24:51 +0100

    Add files via upload

---
+++ local_files/redq1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+
+    def reduce(key, values[])
+        sum, count = 0
+        for value in values:
+            sum += value
+            count += 1
+        emit(sum / count)
