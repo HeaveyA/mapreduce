944424c818df39ff7fb4d899c6c77c7274a44b65
HeaveyA <a.heavey2@gmail.com>
2018-04-15 14:43:30 +0100

    Add files via upload

---
+++ local_files/reducer2A.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 
+# map1.py
+
+import sys
+
+for line in sys.stdin: 
+    keyval=line.strip().split("\t")
+    key,val=(keyval[0],keyval[1])
+    global_max = 0
+    for value in val:
+            if (value > global_max):
+                global_max = value
+            print(global_max)
+           
+	
