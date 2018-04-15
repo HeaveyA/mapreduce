7143912c3c7d22b1b0d71655a40d83e0e098bc46
HeaveyA <a.heavey2@gmail.com>
2018-04-15 14:45:12 +0100

    Add files via upload

--- local_files/reducer2A.py
+++ local_files/reducer2A.py
@@ -5,7 +5,7 @@ import sys
 
 for line in sys.stdin: 
     keyval=line.strip().split("\t")
-    key,val=(keyval[0],keyval[1])
+    key,val=(keyval[0],keyval[0])
     global_max = 0
     for value in val:
             if (value > global_max):
