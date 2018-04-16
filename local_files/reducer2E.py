5ba0d50cc522aa39719401b66dd9f3f7e25f91f4
HeaveyA <a.heavey2@gmail.com>
2018-04-16 09:29:28 +0100

    Add files via upload

--- local_files/reducer2E.py
+++ local_files/reducer2E.py
@@ -6,11 +6,11 @@ import sys
 last_key = None
 max_val = None 
 for line in sys.stdin:
-    key, val = line.strip().split('\t')
+    keyval = line.strip().split('\t')
     if key != last_key:
         print('%s\t%s' % (last_key, max_val))
         max_val = None
-    last_key = key
-    max_val = max(val, max_val)
+    last_key = keyval
+    max_val = max(keyval, max_val)
 if last_key:
      print('%s\t%s' % (last_key, max_val))
