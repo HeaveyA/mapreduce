1106ecc11c1559b3a8b371d85b5f24400efd832c
HeaveyA <a.heavey2@gmail.com>
2018-04-16 11:30:52 +0100

    Add files via upload

--- local_files/reducer2H.py
+++ local_files/reducer2H.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import sys
-index = int(sys.argv[1])
+
 max = 0
 for line in sys.stdin:
     fields = line.strip().split(",")
-    if fields[index].isdigit():
-        val = int(fields[index])
+    if fields.isdigit():
+        val = int(fields)
         if (val > max):
             max = val
         else:
