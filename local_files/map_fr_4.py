ffb9028a5d3ee6503fe03763e7a5af7a1a876b85
HeaveyA <a.heavey2@gmail.com>
2018-04-20 17:06:46 +0100

    Add files via upload

---
+++ local_files/map_fr_4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+
+for line in sys.stdin:
+    user_id, friends = line.split("\t")
+    friend_ids = friends.split(",")
+    
+    for friend in friend_ids:
+        print (user_id + "," + friend, 0)
+    for friend_i in friend_ids:
+        for friend_j in friend_ids:
+            if friend_i == friend_j:
+                continue
+            print '%s%s\t%s' % (frinds_i,friend_j, "1")
