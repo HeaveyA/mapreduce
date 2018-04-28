69485e7b7697f37b79c0f17b04e0e589bd5ae412
HeaveyA <a.heavey2@gmail.com>
2018-04-28 18:57:12 +0100

    Add files via upload

--- local_files/redx11.py
+++ local_files/redx11.py
@@ -4,7 +4,7 @@ import sys
  
 unique_list = []
 for line in sys.stdin:
-	value = line.strip().split('\t')]
+	value = line.strip().split('\t')
     if line not in unique_list:
         unique_list.append(value)
 print(unique_list)
