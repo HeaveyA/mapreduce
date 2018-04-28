84de5ff20c4e77ecc51c22377c4b4c6199f0a05a
HeaveyA <a.heavey2@gmail.com>
2018-04-28 18:55:58 +0100

    Add files via upload

---
+++ local_files/redx11.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+ 
+unique_list = []
+for line in sys.stdin:
+	value = line.strip().split('\t')]
+    if line not in unique_list:
+        unique_list.append(value)
+print(unique_list)
