cb59e5f1ec222202a68ae6655828a91e5fcf575a
HeaveyA <a.heavey2@gmail.com>
2018-05-08 13:49:44 +0100

    Delete red_fr_3.py

--- local_files/red_fr_3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-friend_pair = []
-for line in sys.stdin:
-    values = line.split("\t")
-    if not (0 in values):
-        x = sum(int(values))
-        print (friend_pair, x)
