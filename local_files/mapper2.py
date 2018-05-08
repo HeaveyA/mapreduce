ab03e97102d7ba78534335aaac10e70bed7b1db7
HeaveyA <a.heavey2@gmail.com>
2018-05-08 13:47:44 +0100

    Delete mapper2.py

--- local_files/mapper2.py
+++
@@ -1,6 +0,0 @@
-import sys
-for line in sys.stdin:
-     keyval=line.strip().split("\t")
-     key,val=(keyval[0],keyval[0])
-     if key!="\N" and val!="\N":
-        sys.stdout.write('%s\t%s\n' % (key, val))
